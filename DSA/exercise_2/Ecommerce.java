5c7d05cae3e112acc829367c2238b5c82d237043
JayD2003 <jayasaidodla@gmail.com>
2024-07-31 14:30:05 +0530

    completed the second exercise

--- DSA/exercise_2/Ecommerce.java
+++ DSA/exercise_2/Ecommerce.java
@@ -2,14 +2,25 @@ package exercise_2;
 public class Ecommerce {
     public static void main(String[] args) {
         Product[] products = new Product[]{
-            new Product(1, "Laptop", "Electrical Appliance"),
+            new Product(10, "Laptop", "Electrical Appliance"),
             new Product(2, "Mobile", "Electrical Appliance"),
             new Product(3, "TV", "Electrical Appliance"),
             new Product(4, "Shirts", "Fashion"),
         };
         
-        //Linear Search for the product - Shirts
-        Product searchProduct = SearchingAlgorithms.LinerSearch(products, "Shirts");
-        searchProduct.getDetails();
+        // Linear Search for the product - Shirts
+        Product searchProduct1 = SearchingAlgorithms.linerSearch(products, "Shirts");
+        searchProduct1.getDetails();
+
+        // Binary Search for the product - Laptop
+        Product searchProduct2 = SearchingAlgorithms.linerSearch(products, "Laptop");
+        searchProduct2.getDetails();
+
+        // Sorted Array
+        SearchingAlgorithms.sortProducts(products);
+        System.out.println("Sorted Products");
+        for(Product product : products){
+            product.getDetails();
+        }
     }
 }
--- DSA/exercise_2/SearchingAlgorithms.java
+++ DSA/exercise_2/SearchingAlgorithms.java
@@ -1,6 +1,12 @@
 package exercise_2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SearchingAlgorithms {
-    public static Product LinerSearch(Product[] products, String name){
+
+    //linear search algorithm
+    public static Product linerSearch(Product[] products, String name){
         for(Product product : products){
             if(product.getName().compareTo(name)==0){
                 return product;
@@ -8,6 +14,36 @@ public class SearchingAlgorithms {
         }
         return null;
     }
+     
+    //custom sorting for products using productId
+    public static void sortProducts(Product[] products){
+        Arrays.sort(products, new Comparator<Product>(){
+            public int compare(Product product1, Product product2)
+            {
+                return product1.getId()-product2.getId();
+            }
+        });
+    }
 
-    
+    //binary search algorithm
+    public static Product binarySearch(Product[] products, String name){
+        int start = 0;
+        int end = products.length-1;
+
+        sortProducts(products);
+
+        while(start<end){
+            int mid = (start+end)/2;
+            if(name.compareTo(products[mid].getName())==0){
+                return products[mid];
+            }
+
+            if(name.compareTo(products[mid].getName())>1){
+                mid = end-1;
+            }else{
+                mid = start+1;
+            }
+        }
+        return null;
+    }
 }
